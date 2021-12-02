ce6385eaf5598cacbd46cf7e0a91c2bf96bca171
joshuabl97 <blau.joshua@gmail.com>
2021-12-02 12:36:27 -0500

    env vars

--- problemInput.py
+++ problemInput.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 import requests
 import sys
+import os
+
+sessionToken = os.getenv('AoC_token')
 
 def get_input(day):
     url = "https://adventofcode.com/2021/day/"+str(day)+"/input"
-    headers = {'Cookie': 'session=53616c7465645f5fb0abbb5bbc7c396781eb2cf92514a3c126037b9ea8e9f0b3f3a7a9e7a5ae9d617ee74ac5c067f10e'}
+    headers = {'Cookie': 'session='+sessionToken}
     r = requests.get(url, headers=headers)
 
     if r.status_code == 200:
