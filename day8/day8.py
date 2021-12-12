81d3f96e0a88f43223f24c605fc75c1baba93bd5
joshuabl97 <blau.joshua@gmail.com>
2021-12-12 17:28:26 -0500

    solved all values before seperators

--- day8/day8.py
+++ day8/day8.py
@@ -27,8 +27,18 @@ def problem1(after):
             one += 1
     return(one+four+seven+eight)
 
-def problem2(before):
+def problem2(before, after):
     all_digits = []
+    after_digits = []
+
+    for i in range(len(after)):
+        after_list = after[i].split(' ')
+        nested_digits = []
+        for j in range(len(after_list)):
+            nested_digits.append(list(after_list[j]))
+        after_digits.append(nested_digits)
+
+    print(after_digits)
 
     for i in range(len(before)): 
         before_list = (before[i].split(' '))
@@ -37,6 +47,7 @@ def problem2(before):
         for j in range(len(before_list)):
             nested_digits.append(list(before_list[j]))
         all_digits.append(nested_digits)
+
     for i in range(len(all_digits)):
         ten_digits = all_digits[i] 
         dict_num_keys = [0,1,2,3,4,5,6,7,8,9]
@@ -44,7 +55,7 @@ def problem2(before):
         for i in dict_num_keys:
             dict_nums[i] = None
 
-        dict_value_keys = ['a','b','c','d','e','f','g']
+        dict_value_keys = ['A','B','C','D','E','F','G']
         dict_values = {}
         for i in dict_value_keys:
             dict_values[i] = None
@@ -64,7 +75,7 @@ def problem2(before):
         one_list = (dict_nums[1])
         seven_list = (dict_nums[7])
         li_dif = [i for i in one_list + seven_list if i not in one_list or i not in seven_list]
-        dict_values['a'] = li_dif[0]
+        dict_values['A'] = li_dif[0]
         
         #step 3: the number that has 6 digits and does not contain digits from 1 is number 6
         for j in range(len(ten_digits)):
@@ -79,26 +90,60 @@ def problem2(before):
         a_set = set(dict_nums[1])
         b_set = set(dict_nums[6])
         f_val = list(a_set&b_set)
-        dict_values['f'] = f_val[0]
+        dict_values['F'] = f_val[0]
         for i in range(len(dict_nums[1])):
-            if dict_nums[1][i] != dict_values['f']:
-                dict_values['c'] = dict_nums[1][i]
-        
+            if dict_nums[1][i] != dict_values['F']:
+                dict_values['C'] = dict_nums[1][i]
+
         #step 5: grab unknown vals in 4 and check to see if they exist in 0,9
         ### if it exists in ten_digits[i] == 6 that value is 9
         ### the remaining value in ten_digits[i] == 6 is 0
+        current_values = [i for i in dict_values.values() if i]
+        unknown_four =list(set(dict_nums[4]) - set(current_values))          
+        
+        for j in range(len(ten_digits)):
+            if len(ten_digits[j]) == 6 and ten_digits[j] != dict_nums[6]:
+                if len((list(set(unknown_four) - set(ten_digits[j])))) == 0:
+                    dict_nums[9] = ten_digits[j]
+                else:
+                    dict_nums[0] = ten_digits[j]
 
         #step 6: compare 6,9 the different value that isn't present in 1 is e
-
+        ### if solution doesn't work, start over here
+        six_nine_diff = list(set(dict_nums[6]) - set(dict_nums[9]))
+        dict_values['E'] = six_nine_diff[0]
+        
         #step 7: combine values of 6 & 9 into a single dict, the value that isn't present in 0 is d
+        six_nine_same = list(set(dict_nums[6] + dict_nums[9]))
+        dict_values['D'] = list(set(six_nine_same) - set(dict_nums[0]))[0]
 
         #step 8: the value that isn't present in the values list from 4 is b && the remaining value is g
+        current_values = [i for i in dict_values.values() if i]
+        dict_values['B'] = list(set(dict_nums[4]) - set(current_values))[0]
+        a = list(map(lambda x: x.lower(), dict_value_keys))
+        dict_values['G'] = list(set(a) - set([i for i in dict_values.values() if i]))[0] 
+
+        #step 9: assign the rest of nums using values
+        two = set(map(dict_values.get, ['A','C','D','E','G']))
+        three = list(map(dict_values.get, ['A','C','D','F','G']))
+        five = list(map(dict_values.get, ['A','B','D','F','G']))
+
+        for i in range(len(ten_digits)):
+            if len(ten_digits[i]) == 5:
+                if len(list(set(ten_digits[i])-set(two))) == 0:
+                    dict_nums[2] = ten_digits[i]
+                elif len(list(set(ten_digits[i])-set(three))) == 0:
+                    dict_nums[3] = ten_digits[i]
+                elif len(list(set(ten_digits[i])-set(five))) == 0:
+                    dict_nums[5] = ten_digits[i]
+
         print(dict_values)
         print(dict_nums) 
         print('seperator')
 def main():
     print(problem1(parser()[1]))
-    print(problem2(parser()[0]))
+    before, after = parser()
+    print(problem2(before, after))
 
 if __name__ == '__main__':
     main()
