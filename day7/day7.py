cc9703b8fa9128d8ebdab8a3ec5a37a43c9c4ed3
joshuabl97 <blau.joshua@gmail.com>
2021-12-13 09:42:09 -0500

    brute force method added

--- day7/day7.py
+++ day7/day7.py
@@ -2,7 +2,7 @@
 import statistics
 
 def parser():
-    with open("/Users/jblau407/Code/adventofcode/day7/input.txt", 'r') as f:
+    with open("/Users/jblau407/Code/adventofcode/day7/test.txt", 'r') as f:
         lines = f.readlines()
         numbers = [int(line.strip()) for line in lines[0].split(",")]
     return numbers
@@ -12,9 +12,30 @@ def problem1(nums):
     fuel_consumption = 0
     for i in range(len(nums)):
         fuel_consumption += abs(nums[i]-nums_med)
-    return fuel_consumption
+    return round(fuel_consumption)
 
-def problem2(nums):
+def brute_force(nums):
+    nums.sort() 
+    num_fuel_list = []
+
+    for i in range(len(nums)):
+        local_fuel_list = []
+        for j in range(len(nums)):
+            local_fuel_list.append(abs(nums[i]-nums[j])) 
+        num_fuel_list.append(local_fuel_list) 
+
+    avg_distances = [] 
+    for i in range(len(num_fuel_list)):
+        fuel_avg = 0
+        for j in range(len(num_fuel_list[i])):
+            fuel_avg += num_fuel_list[j][i]
+        avg_distances.append(fuel_avg)
+
+    avg_distances.sort()
+    # this method is more accurate than the method above (brute force checker)
+    return avg_distances[0]
+
+def p2_estimate(nums):
     nums_avg = statistics.mean(nums)
 
     fuel_consumption = 0
@@ -25,12 +46,15 @@ def problem2(nums):
             cost += i+1
         fuel_consumption += cost
 
-    print(nums_avg)
     return fuel_consumption
 
-def main():
-    print(problem1(parser()))
-    print(problem2(parser()))
+def problem2(nums):
+    nums.sort()
+    
 
 if __name__ == "__main__":
-    main()
+    print(f"estimated shortest path: {problem1(parser())}")
+    shortest_distance = brute_force(parser())
+    print(f"shortest path: {shortest_distance}")
+    print(f"part two estimate: {p2_estimate(parser())}")
+    print(f"part two: {problem2(parser())}")
