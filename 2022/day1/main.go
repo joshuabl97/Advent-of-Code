766b09b2ed373db5cc9d29b1cae6cf1e789e1c80
joshuabl97 <blau.joshua@gmail.com>
2022-12-01 10:33:58 -0500

    moved counting of calories to single function

--- 2022/day1/main.go
+++ 2022/day1/main.go
@@ -12,34 +12,9 @@ import (
 
 var filePath = flag.String("f", "input.txt", "path to the input file")
 
-/*
-In case the Elves get hungry and need extra snacks,
-they need to know which Elf to ask: they'd like to know how many
-Calories are being carried by the Elf carrying the most Calories.
-*/
-func problem1(input []int) int {
-	var mostCalories int
-	var currentElf int
-	for _, cals := range input {
-		if cals == 0 {
-			currentElf = 0
-			continue
-		}
-		currentElf += cals
-		if currentElf > mostCalories {
-			mostCalories = currentElf
-		}
-	}
-
-	return mostCalories
-}
-
-/*
-The Elves would instead like to know the total Calories carried by
-the top three Elves carrying the most Calories.
-(The elf to english dictionary is missing a few key words)
-*/
-func problem2(input []int) int {
+// returns a list of the total number of calories that each elf possesses
+// sorted from lowest to highest
+func countCals(input []int) []int {
 	// this is a slice of the total number of calories that each elf possesses
 	var calorieCounts []int
 	var currentElf int
@@ -54,6 +29,24 @@ func problem2(input []int) int {
 
 	sort.Ints(calorieCounts)
 
+	return calorieCounts
+}
+
+/*
+In case the Elves get hungry and need extra snacks,
+they need to know which Elf to ask: they'd like to know how many
+Calories are being carried by the Elf carrying the most Calories.
+*/
+func problem1(calorieCounts []int) int {
+	return calorieCounts[len(calorieCounts)-1]
+}
+
+/*
+The Elves would instead like to know the total Calories carried by
+the top three Elves carrying the most Calories.
+(The elf to english dictionary is missing a few key words)
+*/
+func problem2(calorieCounts []int) int {
 	var totalCals int
 	for i := 1; i <= 3; i++ {
 		totalCals += calorieCounts[len(calorieCounts)-i]
@@ -90,6 +83,7 @@ func parseInput(filePath string) []int {
 func main() {
 	flag.Parse()
 	input := parseInput(*filePath)
-	fmt.Printf("Problem 1: %v \n", problem1(input))
-	fmt.Printf("Problem 2: %v \n", problem2(input))
+	calorieCounts := countCals(input)
+	fmt.Printf("Problem 1: %v \n", problem1(calorieCounts))
+	fmt.Printf("Problem 2: %v \n", problem2(calorieCounts))
 }
