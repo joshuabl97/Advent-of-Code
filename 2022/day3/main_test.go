edb99416c1dec21c1a5289b050c57d97dedda737
joshuabl97 <blau.joshua@gmail.com>
2022-12-03 23:16:08 -0500

    added tests for problem functions

--- 2022/day3/main_test.go
+++ 2022/day3/main_test.go
@@ -99,3 +99,75 @@ func TestNestedStringUnique(t *testing.T) {
 		})
 	}
 }
+
+func TestProblem1(t *testing.T) {
+	testInput := parseInput("testInput.txt")
+	input := parseInput("input.txt")
+
+	t.Parallel()
+
+	type args struct {
+		x [][]string
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "input is testInput.txt",
+			args: args{x: testInput},
+			want: 157,
+		},
+		{
+			name: "input is input.txt",
+			args: args{x: input},
+			want: 8401,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := problem1(tt.args.x); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("problem1() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestProblem2(t *testing.T) {
+	testInput := parseInput("testInput.txt")
+	input := parseInput("input.txt")
+
+	t.Parallel()
+
+	type args struct {
+		x [][]string
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "input is testInput.txt",
+			args: args{x: testInput},
+			want: 70,
+		},
+		{
+			name: "input is input.txt",
+			args: args{x: input},
+			want: 2641,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := problem2(tt.args.x); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("problem2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
