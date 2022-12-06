fe2e9b24d13007eef86962420afbfc937ebfab45
joshuabl97 <blau.joshua@gmail.com>
2022-12-06 14:31:20 -0500

    adding reusable package

---
+++ 2022/reusable/reusable.go
@@ -0,0 +1,53 @@
+package reusable
+
+// returns value in alphabet if given a single digit string
+// a = 1, b = 2, c = 3, A = 27, B = 28, C = 29, etc
+func CharNum(s string) int {
+	char := rune(s[0])
+	if char >= 97 && char <= 122 {
+		return int(char) - 96
+	} else if char >= 65 && char <= 90 {
+		return int(char) - 38
+	}
+	return 0
+}
+
+// checks if there are multiple of the same value in the same array of strings
+// i.e ['a','a'] -> true ['a','b'] -> false
+func DupeExist(arr []string) bool {
+	visited := make(map[string]bool, 0)
+
+	for i := 0; i < len(arr); i++ {
+		if visited[arr[i]] {
+			return true
+		} else {
+			visited[arr[i]] = true
+		}
+	}
+
+	return false
+}
+
+// iterates through a slice of a slice of strings and only returns only unique values on the inner slice
+// i.e [['a','a','b','c','c'], ['a','b','b','c']] returns [['a','b','c'],['a','b','c']]
+func NestedStringUnique(nestedS [][]string) [][]string {
+	for i, s := range nestedS {
+		nestedS[i] = UniqueStrSlice(s)
+	}
+
+	return nestedS
+}
+
+// inputs a slice of strings and returns a string slice containing only unique characters
+// i.e ["a","b","b","b","c","c"] returns ["a","b","c"]
+func UniqueStrSlice(s []string) []string {
+	inResult := make(map[string]bool)
+	var result []string
+	for _, str := range s {
+		if _, ok := inResult[str]; !ok {
+			inResult[str] = true
+			result = append(result, str)
+		}
+	}
+	return result
+}
---
+++ 2022/reusable/reusable_test.go
@@ -0,0 +1,133 @@
+package reusable
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestCharNum(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name string
+		args string
+		want int
+	}{
+		{
+			name: "input is 'p'",
+			args: "p",
+			want: 16,
+		},
+		{
+			name: "input is 'L'",
+			args: "L",
+			want: 38,
+		},
+		{
+			name: "input is 'P'",
+			args: "P",
+			want: 42,
+		},
+		{
+			name: "input is 't'",
+			args: "t",
+			want: 20,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := CharNum(tt.args); got != tt.want {
+				t.Errorf("CharNum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDupeExist(t *testing.T) {
+	t.Parallel()
+	type args struct {
+		arr []string
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "input is ['a', 'b', 'b', 'c']",
+			args: args{arr: []string{"a", "b", "b", "c"}},
+			want: true,
+		},
+		{
+			name: "input is ['a', 'b', 'c']",
+			args: args{arr: []string{"a", "b", "c"}},
+			want: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := DupeExist(tt.args.arr); got != tt.want {
+				t.Errorf("DupeExist() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestNestedStringUnique(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name string
+		args [][]string
+		want [][]string
+	}{
+		{
+			name: "input is [['a','a','b','c','c'], ['a','b','b','c','C']]",
+			args: [][]string{{"a", "a", "b", "c", "c"}, {"a", "b", "b", "c", "c", "C"}},
+			want: [][]string{{"a", "b", "c"}, {"a", "b", "c", "C"}},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NestedStringUnique(tt.args); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NestedStringUnique() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestUniqueStrSlice(t *testing.T) {
+	t.Parallel()
+	type args struct {
+		x []string
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "input is ['a', 'b', 'b', 'c']",
+			args: args{x: []string{"a", "b", "b", "c"}},
+			want: []string{"a", "b", "c"},
+		},
+		{
+			name: "input is ['z', 'z', 'x', 'x', 'y', 'y', 'y']",
+			args: args{x: []string{"z", "z", "x", "x", "y", "y", "y"}},
+			want: []string{"z", "x", "y"},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := UniqueStrSlice(tt.args.x); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("UniqueStrSlice() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
